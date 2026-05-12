ab70aead2d47347f2b167384f0d7c629ead82038
kjetilaas <k.s.aas@geo.uio.no>
2026-05-12 10:39:43 +0200

    Remove nans from missing values

--- calculate_luh2_secondary_agedist.py
+++ calculate_luh2_secondary_agedist.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 from scipy.optimize import curve_fit
-from datetime import date
+from datetime import UTC, date, datetime
 
 def piecewise_exponential_cumulative(ages, k1, k2, m=94):
     ### this function calculates a piecewise exponential function, whose integral equals one
@@ -76,7 +76,7 @@ year_luh2_start = 850
 
 # this is the year that you want to calculate a steady-state logging rate for
 #year_to_calc_steadystate = 2000
-year_to_calc_steadystate = 1700
+year_to_calc_steadystate = 1850
 
 
 print(fin_luh2.dims)
@@ -265,9 +265,13 @@ else:
 fout['time'] = np.arange(n_ts_out)
 fout['YEAR'][:] = np.arange(n_ts_out)
 
+# Set the output encoding for missing values to be -999 instead of the default NaN
+encoding = {var: {'_FillValue': -999.0} for var in fout.data_vars}
+
 
 #fout.to_netcdf('LUH2_states_transitions_management.timeseries_4x5_hist_steadystate_'+str(year_to_calc_steadystate)+'_'+str(date.today())+'.nc')
 #fout.to_netcdf('LUH2_states_transitions_management.timeseries_4x5_hist_steadystate_'+str(year_to_calc_steadystate)+'_'+str(date.today())+'.nc')
-fout.to_netcdf('LUH2_states_transitions_management.timeseries_ne30_hist_steadystate_'+str(year_to_calc_steadystate)+'_'+str(date.today())+'.nc')
-
+#fout.to_netcdf('LUH2_states_transitions_management.timeseries_ne30_hist_steadystate_'+str(year_to_calc_steadystate)+'_'+str(date.today())+'.nc')
+#fout.to_netcdf('LUH3_states_transitions_management.timeseries_ne16_hist_steadystate_'+str(year_to_calc_steadystate)+'_'+str(date.today())+'.nc')
+fout.to_netcdf('LUH3_'+str(year_to_calc_steadystate)+'_steadystate_ne16np4_c'+datetime.now(UTC).strftime('%y%m%d')+'.nc', encoding=encoding)
 
